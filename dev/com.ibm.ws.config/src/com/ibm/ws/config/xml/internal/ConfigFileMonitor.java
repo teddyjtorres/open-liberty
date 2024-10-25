d18394b2006dc27fb1c484782849f7cd886af123
simysc <simysc@ca.ibm.com>
2024-10-25 11:44:58 -0400

    Revert "if server xml is deleted, do not remove configuration"

--- dev/com.ibm.ws.config/src/com/ibm/ws/config/xml/internal/ConfigFileMonitor.java
+++ dev/com.ibm.ws.config/src/com/ibm/ws/config/xml/internal/ConfigFileMonitor.java
@@ -143,11 +143,11 @@ class ConfigFileMonitor implements com.ibm.ws.kernel.filemonitor.FileMonitor {
 
     @Override
     public void onChange(Collection<File> createdFiles, Collection<File> modifiedFiles, Collection<File> deletedFiles) {
-        configRefresher.refreshConfigurationIfServerXMLExists();
+        configRefresher.refreshConfiguration();
     }
 
     @Override
     public void onChange(Collection<File> createdFiles, Collection<File> modifiedFiles, Collection<File> deletedFiles, String filter) {
-        configRefresher.refreshConfigurationIfServerXMLExists();
+        configRefresher.refreshConfiguration();
     }
 }
