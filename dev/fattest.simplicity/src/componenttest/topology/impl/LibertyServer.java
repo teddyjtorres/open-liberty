66638f39f7cbecd91c22653d9642b6704ea9fcf7
Thomas Watson <tjwatson@us.ibm.com>
2024-10-30 14:54:20 -0500

    Remove -Xdump for unexplained NCDFE on the Providers class

--- dev/fattest.simplicity/src/componenttest/topology/impl/LibertyServer.java
+++ dev/fattest.simplicity/src/componenttest/topology/impl/LibertyServer.java
@@ -1669,7 +1669,6 @@ public class LibertyServer implements LogMonitorClient {
         if (info.VENDOR == Vendor.IBM) {
             JVM_ARGS += " -Xdump:system+java+snap:events=throw+systhrow,filter=\"java/lang/ClassCastException#ServiceFactoryUse.<init>*\"";
             JVM_ARGS += " -Xdump:system+java+snap:events=throw+systhrow,filter=\"java/lang/ClassCastException#org/eclipse/osgi/internal/serviceregistry/ServiceFactoryUse.<init>*\"";
-            JVM_ARGS += " -Xdump:system+java+snap:events=throw+systhrow,filter=\"java/lang/NoClassDefFoundError\",msg_filter=\"com.ibm.ws.classloading.internal.providers.Providers\",request=exclusive+prepwalk+serial+preempt";
             JVM_ARGS += " -Xdump:system+java+snap:events=systhrow,filter=\"java/lang/NoSuchMethodError#com/ibm/ws/classloading/internal/AppClassLoader.<init>*\",msg_filter=\"*getPrivateLibraries*\",request=exclusive+prepwalk";
         }
 
