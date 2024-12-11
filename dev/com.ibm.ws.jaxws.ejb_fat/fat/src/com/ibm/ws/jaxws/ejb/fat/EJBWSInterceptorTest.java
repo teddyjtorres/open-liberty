66d7e259e06ccfa6b97b6d794da0496e1fd063b3
Thomas Johnson <tjjohnso@us.ibm.com>
2024-12-11 10:20:06 -0600

    JAX-WS: Add iniital test for application exceptions in interceptors

--- dev/com.ibm.ws.jaxws.ejb_fat/fat/src/com/ibm/ws/jaxws/ejb/fat/EJBWSInterceptorTest.java
+++ dev/com.ibm.ws.jaxws.ejb_fat/fat/src/com/ibm/ws/jaxws/ejb/fat/EJBWSInterceptorTest.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -49,6 +49,7 @@ public class EJBWSInterceptorTest {
     private static final String ejbwsinterceptorear = "EJBWSInterceptor";
 
     private static String SERVICE_ADDRESS = null;
+    private static String EXCEPTION_SERVICE_ADDRESS = null;
 
     private static final String SERVLET_PATH = "/EJBWSInterceptorClient/EJBWSInterceptorTestServlet";
 
@@ -71,6 +72,8 @@ public class EJBWSInterceptorTest {
         }
 
         SERVICE_ADDRESS = new StringBuilder("http://").append(server.getHostname()).append(":").append(server.getHttpDefaultPort()).append("/EJBWSInterceptor/SayHelloService").toString();
+        EXCEPTION_SERVICE_ADDRESS = new StringBuilder("http://").append(server.getHostname()).append(":").append(server.getHttpDefaultPort()).append("/EJBWSInterceptor/SayHelloWithInterceptorExceptionService").toString();
+
         System.out.println("~~SERVICE_ADDRESS: " + SERVICE_ADDRESS);
     }
 
@@ -81,9 +84,6 @@ public class EJBWSInterceptorTest {
         }
     }
 
-    /*
-     * TODO: Fix Java 2 Security exception
-     */
     @Mode(TestMode.FULL)
     @Test
     public void testInterceptor() throws Exception {
@@ -101,6 +101,24 @@ public class EJBWSInterceptorTest {
 
         assertTrue("The response should be '" + expectedValue + "', while the actual is '" + line + "'", expectedValue.equals(line));
         assertNotNull("The expected output in server log is " + expectedTraceOutout, server.waitForStringInLog(expectedTraceOutout));
+    }
+
+    // This test currently hard fails due to unexpected NCDFE, once this is fix, test should be updated to check for expected
+    // UserNotFoundException_Exception
+    @Mode(TestMode.FULL)
+    @Test
+    public void testInterceptorException() throws Exception {
+        String encodedServiceAddress = URLEncoder.encode(EXCEPTION_SERVICE_ADDRESS, "utf-8");
+
+        String servletUrl = new StringBuilder().append("http://").append(server.getHostname()).append(":").append(server.getHttpDefaultPort()).append(SERVLET_PATH).append("?url=").append(encodedServiceAddress).toString();
+        System.out.println("~~servletUrl: " + servletUrl);
+
+        String expectedValue = "hello, EJBWSInterceptor";
+        String expectedTraceOutout = "com.ibm.ws.jaxws.ejbinterceptor.SayHelloInterceptor intercepted the method";
+
+        HttpURLConnection con = HttpUtils.getHttpConnection(new URL(servletUrl), HttpURLConnection.HTTP_SERVER_ERROR, 10);
+        BufferedReader br = HttpUtils.getConnectionStream(con);
+        String line = br.readLine();
     }
 
 }
