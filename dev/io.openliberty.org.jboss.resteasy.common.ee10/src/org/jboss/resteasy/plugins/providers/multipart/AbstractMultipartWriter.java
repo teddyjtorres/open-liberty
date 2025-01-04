b22ca392b649bf88d74e60eb653840743e93fab9
jim-krueger <jckofbyron@gmail.com>
2025-01-04 11:28:10 -0600

    Add RESTEasy 4444 change to address issue

--- dev/io.openliberty.org.jboss.resteasy.common.ee10/src/org/jboss/resteasy/plugins/providers/multipart/AbstractMultipartWriter.java
+++ dev/io.openliberty.org.jboss.resteasy.common.ee10/src/org/jboss/resteasy/plugins/providers/multipart/AbstractMultipartWriter.java
@@ -110,8 +110,13 @@ public class AbstractMultipartWriter {
                 // super.close();
             }
         };
+        //Liberty change start:  Can be removed when https://github.com/resteasy/resteasy/pull/4444 is added
+        final HeaderFlushedOutputStream headerFlushedOutputStream = new HeaderFlushedOutputStream(headers, partStream);
         writer.writeTo(entity, entityType, entityGenericType, annotations, part.getMediaType(), headers,
-                new HeaderFlushedOutputStream(headers, partStream));
+                headerFlushedOutputStream);
+        //Liberty change end
+        // Flush the headers for cases where the entity was empty
+        headerFlushedOutputStream.flushHeaders();
         entityStream.write(LINE_SEPARATOR_BYTES);
     }
 
