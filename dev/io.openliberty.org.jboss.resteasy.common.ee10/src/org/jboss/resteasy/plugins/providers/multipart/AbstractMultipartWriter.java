3243d8929fe38f00de4e1c91bf4d4b57039118cb
jim-krueger <jckofbyron@gmail.com>
2025-01-06 18:09:21 -0600

    Add change for EE11 and fix copyright dates

--- dev/io.openliberty.org.jboss.resteasy.common.ee10/src/org/jboss/resteasy/plugins/providers/multipart/AbstractMultipartWriter.java
+++ dev/io.openliberty.org.jboss.resteasy.common.ee10/src/org/jboss/resteasy/plugins/providers/multipart/AbstractMultipartWriter.java
@@ -1,3 +1,21 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ *
+ * Copyright 2025 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.resteasy.plugins.providers.multipart;
 
 import java.io.IOException;
@@ -110,7 +128,7 @@ public class AbstractMultipartWriter {
                 // super.close();
             }
         };
-        //Liberty change start:  Can be removed when https://github.com/resteasy/resteasy/pull/4444 is added
+        //Liberty change start:  Can be removed when https://github.com/resteasy/resteasy/pull/4443 is added
         final HeaderFlushedOutputStream headerFlushedOutputStream = new HeaderFlushedOutputStream(headers, partStream);
         writer.writeTo(entity, entityType, entityGenericType, annotations, part.getMediaType(), headers,
                 headerFlushedOutputStream);
