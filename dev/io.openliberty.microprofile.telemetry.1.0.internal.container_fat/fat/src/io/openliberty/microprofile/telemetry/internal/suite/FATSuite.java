aaa57776b2bfd77c7e25cc13228399b486acf8dc
yasmin.aumeeruddy <yasmin.aumeeruddy@ibm.com>
2025-01-14 16:16:13 +0000

    Add additional server env variables for Telemetry agent test

--- dev/io.openliberty.microprofile.telemetry.1.0.internal.container_fat/fat/src/io/openliberty/microprofile/telemetry/internal/suite/FATSuite.java
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal.container_fat/fat/src/io/openliberty/microprofile/telemetry/internal/suite/FATSuite.java
@@ -39,11 +39,11 @@ import io.openliberty.microprofile.telemetry.internal.tests.MetricsApiOtelCollec
 @RunWith(Suite.class)
 @SuiteClasses({
                 AlwaysPassesTest.class, //Must keep this test to run something in the Java 6 builds.
-                //AgentTest.class,
-                //Agent129Test.class,
-                //Agent210Test.class,
+                AgentTest.class,
+                Agent129Test.class,
+                Agent210Test.class,
                 AgentConfigTest.class,
-                /*AgentConfigMultiAppTest.class,
+                AgentConfigMultiAppTest.class,
                 CrossFeatureJaegerTest.class,
                 CrossFeatureZipkinTest.class,
                 JaegerSecureOtelCollectorTest.class,
@@ -55,7 +55,7 @@ import io.openliberty.microprofile.telemetry.internal.tests.MetricsApiOtelCollec
                 JvmMetricsOtelCollectorTest.class,
                 MetricsApiOtelCollectorTest.class,
                 ZipkinOtelCollectorTest.class,
-                ZipkinTest.class,*/
+                ZipkinTest.class,
 
 })
 
