0e54250cbbdb493521cad302f22aab6357786d78
Wilalberto Rodriguez <wrodrig@us.ibm.com>
2025-01-24 11:32:54 -0600

    Set the treadClientID in the AccessTokenAuthenticator

--- dev/com.ibm.ws.security.openidconnect.clients.common/src/com/ibm/ws/security/openidconnect/clients/common/OIDCClientAuthenticatorUtil.java
+++ dev/com.ibm.ws.security.openidconnect.clients.common/src/com/ibm/ws/security/openidconnect/clients/common/OIDCClientAuthenticatorUtil.java
@@ -466,7 +466,6 @@ public class OIDCClientAuthenticatorUtil {
     public static String getIssuerIdentifier(ConvergedClientConfig clientConfig) {
         String issuer = null;
         issuer = clientConfig.getIssuerIdentifier();
-        setThreadClientId(clientConfig.getClientId());
         if (issuer == null || issuer.isEmpty()) {
             issuer = extractIssuerFromTokenEndpointUrl(clientConfig);
         }
@@ -490,7 +489,7 @@ public class OIDCClientAuthenticatorUtil {
         return issuer;
     }
 
-    private static void setThreadClientId(String clientID) {
+    private void setThreadClientId(String clientID) {
         threadClientID.set(clientID);
     }
 
