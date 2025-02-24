52d66beffd0f19509b87994deae0ab62a01f9703
Andrew Rouse <anrouse@uk.ibm.com>
2025-02-24 17:40:40 +0000

    Test InvocationContext sees bindings from Ext
    Ensure that annotation bindings added by an extension can be retrieved
    from the InvocationContext.

--- dev/com.ibm.ws.cdi.extension_fat/fat/src/com/ibm/ws/cdi/extension/apps/invocationContext/MyAddedByExtensionBinding.java
+++ dev/com.ibm.ws.cdi.extension_fat/fat/src/com/ibm/ws/cdi/extension/apps/invocationContext/MyAddedByExtensionBinding.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2025 IBM Corporation and others.
+ * Copyright (c) 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package com.ibm.ws.cdi.extension.apps.invocationContext;
 
@@ -20,9 +17,11 @@ import java.lang.annotation.Target;
 import javax.interceptor.InterceptorBinding;
 
 /**
- * Basic interceptor binding
+ * Binding annotation added to test class and method by an extension
  */
 @InterceptorBinding
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface MyInterceptorBinding {}
+public @interface MyAddedByExtensionBinding {
+
+}
